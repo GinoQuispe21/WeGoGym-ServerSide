510f0c5ee2624dcedc7568632e348984b63eb892
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2022-12-08 21:32:26 -0500

    create project

---
+++ wegogym/src/test/java/com/ginoquispe/wegogym/WegogymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ginoquispe.wegogym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WegogymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
